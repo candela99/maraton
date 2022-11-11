96466560e0c4b64b4d2fdcfa86a28eec2fcea548
Dario Gonzalez <odariog94@gmail.com>
2022-11-11 17:50:57 -0300

    Agregado mapa de centros medicos

--- maraton/js/mapa.js
+++ maraton/js/mapa.js
@@ -5,3 +5,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+let map2 = L.map('map2').setView([-34.522832, -58.700531],13) //coordenadas de la UNGS
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map2);
+
+L.marker([-34.541941473196005, -58.71009050056615],{draggable: false}).addTo(map2)
+    .bindPopup('<b>Centro de salud N°1</b><br>Direccion 0001<br><i>Horario: 8-13</i>');
+	
+L.marker([-34.54240006289364, -58.71129542632785],{draggable: false}).addTo(map2)
+    .bindPopup('<b>Centro de salud N°2</b><br>Direccion 0002<br><i>Horario: 8-13</i>');
+
+L.marker([-34.5386154585882, -58.70007194211304],{draggable: false}).addTo(map2)
+    .bindPopup('<b>Centro de salud N°3</b><br>Direccion 0003<br><i>Horario: 8-13</i>');
+
+L.marker([-34.57033889693274, -58.72710821406872],{draggable: false}).addTo(map2)
+    .bindPopup('<b>Centro de salud N°4</b><br>Direccion 0004<br><i>Horario: 8-13</i>');	
+	
+L.marker([-34.542462516578325, -58.73393781731669],{draggable: false}).addTo(map2)
+    .bindPopup('<b>Centro de salud N°5</b><br>Direccion 0005<br><i>Horario: 8-13</i>');	
+
